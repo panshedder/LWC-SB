90e8fb86a2775ff7a921f07d012feafa9e2bec5a
Francisco Liu <fliug@deloitte.com>
2022-03-28 09:36:37 -0300

    C5 done: Build component boatSearchForm

--- force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -16,6 +16,10 @@ export default class BoatSearchForm extends LightningElement {
         this.searchOptions = data.map(type => {
           // TODO: complete the logic
           // [{ label: 'New', value: 'new' },... ]
+          return { //REVISAR
+            label: type.Name,
+            value: type.Id
+          }
         });
         this.searchOptions.unshift({ label: 'All Types', value: '' });
       } else if (error) {
@@ -27,9 +31,10 @@ export default class BoatSearchForm extends LightningElement {
     // Fires event that the search option has changed.
     // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
     handleSearchOptionChange(event) {
+      this.selectedBoatTypeId = event.detail.value; //REVISAR
       // Create the const searchEvent
       // searchEvent must be the new custom event search
-      searchEvent;
+      const searchEvent = new CustomEvent('search', {detail: {boatTypeId: this.selectedBoatTypeId}});
       this.dispatchEvent(searchEvent);
     }
   }
